cb09592bda946354c3c7da20739761be7bdbe49b
Toivo Henningsson <toivo.h.h@gmail.com>
2012-04-30 13:32:59 +0200

    Pulled @setdefault into utils.jl.

---
+++ dataflow/utils.jl
@@ -0,0 +1,32 @@
+
+
+is_expr(ex, head::Symbol) = (isa(ex, Expr) && (ex.head == head))
+function expect_expr(ex, head::Symbol)
+    if !is_expr(ex, head)
+        error("expected expr(:$head,...), found $ex")
+    end
+end
+
+# macro setdefault(args...)
+#     # allow @setdefault(refexpr, default)
+#     if (length(args)==1) && is_expr(args[1], :tuple)
+#         args = args[1].args
+#     end
+#     refexpr, default = tuple(args...)
+macro setdefault(refexpr, default)
+    expect_expr(refexpr, :ref)
+    dict_expr, key_expr = tuple(refexpr.args...)
+    @gensym dict key #defval
+    quote
+        ($dict)::Associative = ($dict_expr)
+        ($key) = ($key_expr)
+        if has(($dict), ($key))
+            ($dict)[($key)]
+        else
+            ($dict)[($key)] = ($default) # returns the newly inserted value
+#             ($defval) = ($default)
+#             println("defval: ", ($defval))
+#             ($dict)[($key)] = ($defval) # returns the newly inserted value
+        end
+    end
+end
