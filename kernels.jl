389fa0f2377b068fdc41181f4ccbff570cc6893e
Toivo Henningsson <toivo@control.lth.se>
2012-05-02 15:27:36 +0200

    Wrapped flatten slightly inside kernels.jl, moved dataflow/* to ..

--- kernels.jl
+++ kernels.jl
@@ -17,9 +17,8 @@ function wrap_kernel_body(flat_code::Vector, indvars)
     body
 end
 
-function wrap_kernel(context::Context, flat_code::Vector, indvars, 
+function wrap_kernel(arguments::Vector, flat_code::Vector, indvars, 
                      staged::Bool)
-    arguments = append(context.outputs, context.inputs)
     fname = gensym("kernel")
     signature = expr(:call, fname, arguments...)
 
@@ -35,22 +34,28 @@ function wrap_kernel(context::Context, flat_code::Vector, indvars,
     return :($fdef; $fname)
 end
 
-function make_kernel(nd, code)
-    # flatten the code
+function flatten_kernel(code::Expr)
     symbols = Dict{Symbol,SymEntry}()
     flat_code = {}
     receive = ex->append!(flat_code, {ex})
     context = Context(symbols, receive)
     value = flatten(context, code)
 
+    arguments = append(context.outputs, context.inputs)
+    
+    flat_code, arguments
+end
+
+function make_kernel(nd, code)
+    # flatten the code
+    flat_code, arguments = flatten_kernel(code)
+
     #indvars = gensym(32) # just some number of dims that should be enough
     indvars = (:_i, :_j, :_k, :_l) # todo: use gensyms instead
     
     staged = true
-    fdef = wrap_kernel(context, flat_code, indvars[1:nd], staged)
+    fdef = wrap_kernel(arguments, flat_code, indvars[1:nd], staged)
     kernel = eval(fdef)
-
-    arguments = append(context.outputs, context.inputs)
 
     expr(:call, kernel, arguments...)
 end
