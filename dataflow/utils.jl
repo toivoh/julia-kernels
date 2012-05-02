c9c36a973dd74e86f55b39e668012849bfbbc646
Toivo Henningsson <toivo@control.lth.se>
2012-05-02 13:18:00 +0200

    Added some comments and optional return type specification to @cached.

--- dataflow/utils.jl
+++ dataflow/utils.jl
@@ -88,12 +88,19 @@ function wrap_cached(func::Expr)
     context = peel_typeassert(context_decl)
     context_type = context_decl.args[2]
 
+    # extract argument names tuple for args[2:end]
     restargs = { peel_typeassert(arg) | arg in signature.args[3:end] }
     restargs = expr(:tuple, restargs)
 
+    return_type = (if is_expr(body, doublecolon)
+        @expect length(body.args) == 2; body.args[2]
+    else; Any; end)
+
+    # create key to identify this method in the cache
     fname = signature.args[1]
     methodkey = quote_expr(gensym(string(fname)))
 
+    # wrap the body with lookup and storage into cache
     body = quote
         # find result cache for this method in context
         mcache = ($context).cache
@@ -105,22 +112,23 @@ function wrap_cached(func::Expr)
         # return cached result if available
         restargs = ($restargs)
         if has(cache, restargs)
-            return cache[restargs]
+            return cache[restargs]::($return_type)
         end
         
         # evaluate original body, store result and return
         value = let
-            ($body)
+            ($body)  # ::($return_type) that's where we took it from
         end
         cache[restargs] = value
         value
     end
 
+    # new function definition
     newfun = expr(:function, signature, body)
 
-    context_type_err_msg = strcat("\@cached $signature: first argument should be ",
-        "of type C <: Context, got ",
-        string(context_decl))
+    # add a check to see that the context argument type <: Context
+    context_type_err_msg = strcat("\@cached $signature: first argument should",
+        " be of type C <: Context, got ", string(context_decl))
     quote
         if !($context_type <: $Context)
             error($context_type_err_msg)
