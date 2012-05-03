ffd7577c59f9a09666ab29cc49f9483a6f3ec34a
Toivo Henningsson <toivo@control.lth.se>
2012-05-03 15:41:35 +0200

    Tidied up dag.jl

--- dag.jl
+++ dag.jl
@@ -1,15 +1,19 @@
 
+# dag.jl:
+# ======
+# Node types for DAG
+#
+
 load("utils/utils.jl")
 
 
-# == Node =====================================================================
-
 abstract Expression
+
+# == Node =====================================================================
 
 type Node{T<:Expression}
     val::T
-#    args::Vector{Node}
-    args
+    args#::Vector{Node} # todo: add back once type inference bug is fixed
 
     name::Union(Symbol,Nothing)
     num_uses::Int
@@ -26,17 +30,12 @@ type Node{T<:Expression}
         error("Node{$T}: No constructor for val = $val, args=$args")
     end
 
-    # Used to forward typealias constructors to Node(T, args...)
-#    Node(targs...) = Node(T, targs...)
-    function Node(targs...)
-        #println("Node{$T}($targs)")
-        Node(T, targs...)
-    end
+    # forwards typealias constructors to Node(T, args...)
+    Node(targs...) = Node(T, targs...)
 end
 
-Node{T<:Expression}(val::T, cargs...) = Node{T}(val, cargs...)
-
-function Node{T<:Expression}(node::Node{T}, args) 
+Node         {T<:Expression}(val::T, cargs...) = Node{T}(val, cargs...)
+function Node{T<:Expression}(node::Node{T}, args) # copy node, but use new args
     newnode = Node(node.val, args)
     newnode.name = node.name
     newnode
@@ -45,8 +44,9 @@ end
 typealias Nodes Vector{Node}
 
 
-# -- expressions --------------------------------------------------------------
+# == Expressions ==============================================================
 
+## terminals ##
 abstract Terminal      <: Expression
 type       NoEx        <: Terminal; end
 type       LiteralEx   <: Terminal; value; end
@@ -56,6 +56,7 @@ type       SymbolEx    <: Terminal;
     SymbolEx(name::Symbol, kind::Symbol) = new(name, kind)
 end
 
+## nonterminals ##
 abstract Nonterminal   <: Expression
 abstract   Operation   <: Nonterminal # Nonterminal with storable value
 abstract     FuncOp    <: Operation     # Operation without side effects
@@ -67,6 +68,9 @@ type           RefEx     <: FuncOp;  end
 abstract     Action    <: Operation     # Operation with side effects
 type           AssignEx <: Action;   end
 type       EllipsisEx    <: Nonterminal; end
+
+
+## Node type aliases ##
 
 typealias TerminalNode{T<:Terminal}    Node{T}
 typealias NontermNode {T<:Nonterminal} Node{T}
@@ -88,6 +92,8 @@ typealias EllipsisNode Node{EllipsisEx}
 typealias AssignNode   Node{AssignEx}
 
 
+## Node type dependent methods: constructors, checker, getters ##
+ 
 Node      {T<:Terminal}(::Type{T}, targs...) = Node{T}(T(targs...), ())
 check_args{T<:Terminal}(node::Node{T}) = (@expect length(node.args) == 0)
 
@@ -95,14 +101,13 @@ Node      {T<:Nonterminal}(::Type{T}, args...) = Node{T}(T(), args)
 get_args  {T<:Nonterminal}(node::Node{T}) = node.args
 check_args{T<:Nonterminal}(::Node{T}) = nothing
 
+get_op       (node::CallNode) = node.args[1]
+get_callargs (node::CallNode) = node.args[2:end]
+check_args   (node::CallNode) = (@expect length(node.args) >= 1)
 
-get_op      (node::CallNode) = node.args[1]
-get_callargs(node::CallNode) = node.args[2:end]
-check_args  (node::CallNode) = (@expect length(node.args) >= 1)
-
-get_A     (node::RefNode) = node.args[1]
-get_inds  (node::RefNode) = node.args[2:end]
-check_args(node::RefNode)  = (@expect length(node.args) >= 1)
+get_A        (node::RefNode) = node.args[1]
+get_inds     (node::RefNode) = node.args[2:end]
+check_args   (node::RefNode)  = (@expect length(node.args) >= 1)
 
 check_args(node::KnotNode) = (@expect length(node.args) >= 1)
 
