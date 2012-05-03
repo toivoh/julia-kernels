dcc7362935de6eec7b074a79bc77ca218cee45ba
Toivo Henningsson <toivo@control.lth.se>
2012-05-03 14:28:10 +0200

    wrote @failexpect macro.

---
+++ test_expect.jl
@@ -0,0 +1,22 @@
+
+load("utils.jl")
+
+macro evalcatch(ex)
+    quote; let
+        println($string(ex))
+        try
+            value = $ex
+            println("  success; returned ", value)
+        catch err
+            println("  threw \"", err, '"')
+        end
+        println()
+    end; end
+end
+
+@evalcatch @expect true
+@evalcatch @expect false
+
+@evalcatch @expect is_expr(:(x+y), :call)
+@evalcatch @expect is_expr(1, :call)
+@evalcatch @expect is_expr(1)
--- utils.jl
+++ utils.jl
@@ -1,34 +1,109 @@
 
-quote_expr(ex) = expr(:quote, ex)
+macro show(ex)
+    :(println(($string(ex)), " = ", $ex) )
+end
+
+
+quoted_expr(ex) = expr(:quote, ex)
+quoted_tuple(t) = expr(:tuple, {t...})
 
 is_expr(ex, head::Symbol) = (isa(ex, Expr) && (ex.head == head))
+
+
+# == @expect ==================================================================
+
+fail_expect(predexpr) = error("Expected: ", string(predexpr))
+
+default_checkexpect_code(ex) = :($ex ? nothing : fail_expect($quoted_expr(ex)))
+make_checkexpect_code(ex)    = default_checkexpect_code(ex)
 
 macro expect(args...)
     if !(1 <= length(args) <= 2)
         error("\n@expect: expected one or two arguments")
     end
-    predicate = args[1]
+    predexpr = args[1]
     if length(args) >= 2
+        # explicit error message given
         message = args[2]
-
-        if is_expr(message, :tuple)
-            msg_parts = message.args
-        else
-            msg_parts = {message}
-        end
+        msg_parts = is_expr(message, :tuple) ? message.args : {message}
+        return :(($predexpr) ? nothing : ($expr(:call, :error, msg_parts...)))
     else
-        msg_parts = {"Expected: ", string(predicate)}
+        # no explicit message
+        return make_checkexpect_code(predexpr)
     end    
-    :($predicate ? nothing : $(expr(:call, :error, msg_parts...)))
 end
 
-macro show(ex)
-    :(println(($string(ex)), " = ", $ex) )
+
+# == split_fdef ===============================================================
+
+function split_fdef(fdef::Expr)
+    @expect (fdef.head == :function) || (fdef.head == :(=))
+    @expect length(fdef.args) == 2
+    signature, body = tuple(fdef.args...)
+    @expect is_expr(signature, :call)
+    @expect length(signature.args) >= 1
+    (signature, body)
 end
+split_fdef(f::Any) = error("split_fdef: expected function definition, got\n$f")
+
+
+# == @failexpect ==============================================================
+
+const failexp_handlers = Dict{Function,Function}()
+
+# add failed expect message generator, e g
+# @failexpect is_expr(ex, head) = "expected expr(:$head,...), got $ex"
+macro failexpect(fdef)
+    signature, body = split_fdef(fdef)
+    f, args = signature.args[1], signature.args[2:end]
+
+    quote; let 
+        const handler = get_failexp_handler(($f), ($string(f)))
+        handler($args...) = $body
+    end; end
+end
+function get_failexp_handler(f::Function, fname::String)
+    if !has(failexp_handlers, f)
+        hname = gensym("failexpect_handler_for__"*fname)
+        failexp_handlers[f] = @eval begin
+            ($hname)(args...) = nothing
+            ($hname)
+        end        
+    end
+    failexp_handlers[f]
+end
+
+function fail_expect(predexpr, pred::Function, args::Tuple)
+    if has(failexp_handlers, pred); msg = failexp_handlers[pred](args...) 
+    else                            msg = nothing; end
+
+    is(msg, nothing) ? fail_expect(predexpr) : error(msg)
+end
+
+function make_checkexpect_code_call(predexpr::Expr)
+    @assert predexpr.head == :call
+    pred, args = predexpr.args[1], predexpr.args[2:end]
+    qpredexpr =  quoted_expr(predexpr)
+
+    quote; let args = ($quoted_tuple(args))
+        ($pred)(args...) ? nothing : fail_expect(($qpredexpr), ($pred), args)
+    end; end
+end
+# tie into @expect
+function make_checkexpect_code(ex::Expr)
+    if is_expr(ex, :call); make_checkexpect_code_call(ex)
+    else;                  default_checkexpect_code(ex);   end
+end
+
+
+
+@failexpect is_expr(ex, head::Symbol) = "expected expr(:$head,...), got $ex"
 
 function expect_expr(ex, head::Symbol)
     @expect is_expr(ex, head) "expected expr(:$head,...), found $ex"
 end
+
+# == @setdefault ==============================================================
 
 # macro setdefault(args...)
 #     # allow @setdefault(refexpr, default)
@@ -47,9 +122,6 @@ macro setdefault(refexpr, default)
             ($dict)[($key)]
         else
             ($dict)[($key)] = ($default) # returns the newly inserted value
-#             ($defval) = ($default)
-#             println("defval: ", ($defval))
-#             ($dict)[($key)] = ($defval) # returns the newly inserted value
         end
     end
 end
