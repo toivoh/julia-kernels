80d900d54d7a87ae5f67b393c51be231d9a255ff
Toivo Henningsson <toivo@control.lth.se>
2012-05-04 14:14:28 +0200

    Tested scanvars and rewrote it to handle more cases correctly.

--- test/test_scanvars.jl
+++ test/test_scanvars.jl
@@ -1,9 +1,40 @@
 
 load("utils/namespace.jl")
 
-show(scanvars(quote
-    x::Int
-    y
-    z=1
-    local w::Float
-    end))
+showall(scanvars(quote
+    a
+    b::Int
+    c=1
+    d::Int = 2
+    local e
+    local f::Int
+    local g=1
+    local h::Int = 2
+
+    x, y::Float = 1, 2
+    begin
+        z=3
+    end
+    let q=1
+        local f::String="f"
+        r::Int = 3
+    end
+
+    k = :k
+    local k::Symbol
+
+    function f(x)
+        local e
+    end
+    f(x,y) = (local z=x*y)
+
+    abstract S
+    abstract T <: S
+    type U
+        x::Uint
+        y::Uint
+    end
+    type V <: T
+        z::String
+    end
+end))
--- utils/namespace.jl
+++ utils/namespace.jl
@@ -14,7 +14,7 @@ type ScannedVars
 end
 
 function addsym(c::ScannedVars, name::Symbol, typename::Symbol, inlocal::Bool)
-    println("addsym(inlocal=$inlocal): (name, typename) = ($name, $typename)")
+#    println("addsym(inlocal=$inlocal): (name, typename) = ($name, $typename)")
 
     setvar = inlocal
     if !has(c.vartypes, name)
@@ -43,12 +43,12 @@ end
 
 scanvars(c::ScannedVars, ex) = scanvars(c, ex, false)
 
-scanvars(::ScannedVars, ::Any, ::Bool) = nothing
-function scanvars(c::ScannedVars, ex::Expr, inlocal::Bool)
-    println("scanvars(inlocal=$inlocal): ex=$ex")
+scanvars(::ScannedVars, ::Any) = nothing
+function scanvars(c::ScannedVars, ex::Expr)
+#     println("scanvars: ex=$ex")
 
     # don't recurse into scope forming exprs
-    if contains([:for, :while, :try, :let, :type, :(->), :function], ex.head)
+    if contains([:for, :while, :try, :let, :(->), :function], ex.head)
         return
     end
     if ex.head == :global; return; end
@@ -57,10 +57,11 @@ function scanvars(c::ScannedVars, ex::Expr, inlocal::Bool)
 
     # still have to watch out for e g f(x)=x^2
     if ex.head == :(=)
-        scanvars_lhs(c, ex.args[1], inlocal)
+        scanvars_lhs(c, ex.args[1], false)
         scanvars(c, ex.args[2])
     elseif ex.head == :local
-        foreach(arg->scanvars(c, arg, true), ex.args)
+#        println("scanvars: ex = ", ex)
+        foreach(arg->scanvars_local(c, arg), ex.args)
     elseif contains([:type, :abstract], ex.head)
         scanvars_typename(c, ex.args[1])
     else
@@ -70,7 +71,7 @@ end
 
 scanvars_lhs(c::ScannedVars, lhs::Symbol, il::Bool) = addsym(c, lhs, :Any, il)
 function scanvars_lhs(c::ScannedVars, lhs::Expr, inlocal::Bool)
-    println("scanvars_lhs(inlocal=$inlocal): lhs=$lhs")
+#     println("scanvars_lhs(inlocal=$inlocal): lhs=$lhs")
 
     if lhs.head == :tuple
         foreach(arg->scanvars_lhs(c, arg, inlocal), lhs.args)
@@ -83,12 +84,23 @@ function scanvars_lhs(c::ScannedVars, lhs::Expr, inlocal::Bool)
     end
 end
 
-scanvars_typename(c::ScannedVars, name::Symbol) = addsym(c, name, name)
-function scanvars_typename(c::ScannedVars, ex::Expr)
-    println("scanvars_typename: ex=$ex")
+scanvars_local(c::ScannedVars, lhs::Symbol) = scanvars_lhs(c, lhs, true)
+function scanvars_local(c::ScannedVars, ex::Expr)
+#    println("scanvars_local: ex = $ex")
+    if ex.head == :(=)
+        scanvars_lhs(c, ex.args[1], true)
+        scanvars(c, ex.args[2])
+    else
+        scanvars_lhs(c, ex, true) # assume it's a ::
+    end
+end
 
-    if !((ex.head == comparison) && (length(ex.args) == 2))
+scanvars_typename(c::ScannedVars, name::Symbol) = addsym(c, name, :Type, true)
+function scanvars_typename(c::ScannedVars, ex::Expr)
+#     println("scanvars_typename: ex=$ex")
+
+    if !((ex.head == :comparison) && (length(ex.args) == 3))
         error("scanvars: invalid type signature ", ex)
     end
-    scanvars_typename(ex.args[1])
+    scanvars_typename(c, ex.args[1])
 end
