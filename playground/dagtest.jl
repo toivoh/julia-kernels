666bc9d06e04fba21a8a4a967a16916eea96119f
Toivo Henningsson <toivo.h.h@gmail.com>
2012-05-13 18:29:03 +0200

    Simplified dagtest.jl a little.

--- playground/dagtest.jl
+++ playground/dagtest.jl
@@ -3,22 +3,14 @@ load("utils/req.jl")
 req("unify/unify.jl")
 req("prettyshow/prettyshow.jl")
 
-type NodeType
-    kind::Symbol
-end
 
-show(io, t::NodeType) = print(io, t.kind)
-
-
-pshow(io::PrettyIO, t::Tuple) = pshow_comma_list(io, {t...}, "\n(", ")\n")
+pshow(io::PrettyIO, t::Tuple) = pshow_comma_list(io, {t...}, "\n(", ")")
 
 
 
-calltype, reftype, inputtype = map(NodeType, (:call, :ref, :input))
-
-callnode(args...) = tuple(calltype, args...)
-refnode(args...) = tuple(reftype, args...)
-inputnode(arg) = tuple(inputtype, arg)
+callnode(args...) = tuple(:call, args...)
+refnode(args...) = tuple(:ref, args...)
+inputnode(arg) = tuple(:input, arg)
 
 #X = A.*B .+ C
 
