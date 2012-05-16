cddeb4abed75748b83ab7b971996fe7cbbf65aea
Toivo Henningsson <toivo.h.h@gmail.com>
2012-05-16 06:51:52 +0200

    Started to rewrite recode_pattern.

--- pdispatch/meta.jl
+++ pdispatch/meta.jl
@@ -16,31 +16,57 @@ split_fdef(f::Any) = error("split_fdef: expected function definition, got\n$f")
 
 # -- recode_pattern -----------------------------------------------------------
 
+type RPVarEntry
+    name::Symbol   
+    T_expr
+    temp_name::Symbol
+end
+function code_create_pvar(e::RPVarEntry)
+    :( ($e.temp_name)=pvar(($quotevalue(t.name)), ($T_expr)) )
+end
+
 type RPContext
-    vars::Dict{Symbol,PVar}
+    vars::Dict{Symbol,RPVarEntry}
     unmatchable::Bool # todo: use!
 
     RPContext() = new(Dict{Symbol,PVar}(), false)
 end
 
-getvar(c::RPContext, name::Symbol) = getvar(c, name, Any)
-function getvar(c::RPContext, name::Symbol, T)
+#getvar(c::RPContext, name::Symbol) = getvar(c, name, quotevalue(Any))
+getvar(c::RPContext, name::Symbol) = getvar(c, name, :Any)
+function getvar(c::RPContext, name::Symbol, T_expr)
     if has(c.vars, name)
-        var = c.vars[name]
-        @expect isequal(pattype(var), T)
-        return var
+        entry = c.vars[name]
+        if !isequal(T_expr, entry.T_expr)
+            error("conflicting types in pattern: $(name)::$(entry.T_expr) "
+                  *" vs $(name)::$(T_expr)")
+        end
     else
-        var = PVar(name, T)
-        return c.vars[name] = var
+        entry = RPVarEntry(name, T_expr, gensym(string(name)))
+        c.vars[name] = entry
+    end
+    entry.temp_name
+end
+
+
+function recode_pattern(ex)
+    rpc = RPContext()
+    pattern_ex = recode_pattern(rpc, ex)
+    
+    quote
+        let ($pvar_defs...)
+            ($pattern_ex)
+        end
     end
 end
+
 
 function recode_pattern(c::RPContext, ex::Expr)
     head, args = ex.head, ex.args
     nargs = length(args)
     if head == doublecolon
         @expect nargs==2
-        return quoted_expr(getvar(c, args[1], args[2]))
+        return getvar(c, args[1], args[2])
     elseif contains([:call, :ref, :curly], head)
         if (head==:call) && (args[1]==:value)
             @expect nargs==2
@@ -53,5 +79,5 @@ function recode_pattern(c::RPContext, ex::Expr)
         return expr(head, {recode_pattern(c,arg) for arg in ex.args})
     end
 end
-recode_pattern(c::RPContext, sym::Symbol) = quoted_expr(getvar(c, sym))
+recode_pattern(c::RPContext, sym::Symbol) = getvar(c, sym)
 recode_pattern(c::RPContext, ex) = ex # other terminals
--- playground/dagtest.jl
+++ playground/dagtest.jl
@@ -1,9 +1,10 @@
 
 load("utils/req.jl")
-#req("unify/unify.jl")
-#req("unify/pmatch.jl")
-load("unify/pmatch.jl")
 req("prettyshow/prettyshow.jl")
+
+#req("pdispatch/pdispatch.jl")
+load("pdispatch/pdispatch.jl")
+load("pdispatch/ifmatch.jl")
 
 
 pshow(io::PrettyIO, t::Tuple) = pshow_comma_list(io, {t...}, "\n(", ")")
@@ -13,45 +14,19 @@ refnode(args...) = tuple(:ref, args...)
 inputnode(arg) = tuple(:input, arg)
 scatternode(arg) = tuple(:scatter, arg)
 
-# @pattern scprop(scatternode(s)) = propsc(s)
-# @pattern function scprop(node) 
-#     tuple(node[1:2]..., {scprop(arg) for arg in node[3:end]}...)
-# end
-
-# @pattern function propsc(callnode(op, arg1, arg2))
-#     callnode(op, propsc(arg1), propsc(arg2))
-# end
-# @pattern propsc(inputnode(name)) = refnode(inputnode(name), :(...))
-# @pattern function propsc(node)
-#     tuple(node[1], {scatterprop(scatter(arg)) for arg in node[2:end]}...)
-# end
-
-
-function scatterprop(node)
-    newnode::Any
-    if !@ifmatch let scatternode(s)=node
-        newnode = propscatter(s)
-    end
-        newnode = tuple(node[1:2]..., 
-                        {scatterprop(arg) for arg in node[3:end]}...)
-    end
-    newnode
+@pattern scprop(scatternode(s)) = propsc(s)
+@pattern function scprop(node::Tuple) 
+    tuple(node[1:2]..., {scprop(arg) for arg in node[3:end]}...)
 end
 
-function propscatter(node)
-    newnode::Any
-    if !@ifmatch let callnode(op, arg1, arg2)=node
-        newnode = callnode(op, propscatter(arg1), propscatter(arg2))
-    end
-        if !@ifmatch let inputnode(name)=node
-            newnode = refnode(inputnode(name), :(...))
-        end
-            newnode = tuple(node[1], 
-                        {scatterprop(scatter(arg)) for arg in node[2:end]}...)
-        end
-    end
-    newnode
+@pattern function propsc(callnode(op, arg1, arg2))
+    callnode(op, propsc(arg1), propsc(arg2))
 end
+@pattern propsc(inputnode(name)) = refnode(inputnode(name), :(...))
+@pattern function propsc(node:Tuple)
+    tuple(node[1], {scatterprop(scatter(arg)) for arg in node[2:end]}...)
+end
+
 
 
 #X = A.*B .+ C
@@ -74,48 +49,8 @@ for node in {A, B, C, AB, X}
     end
 end
 
+
+
 pprintln()
-spex = scatterprop(scatternode(X))
+spex = scprop(scatternode(X))
 pprintln(spex)
-
-
-mt_scprop = PatternMethodTable()
-#add(mt_scprop, :(scatternode(s)), :(propsc(s)))
-add(mt_scprop, :(scatternode(s)), quote
-    println("nlk")
-    propsc(s)
-end)
-add(mt_scprop, :(node), 
-    quote
-        tuple(node[1], {scprop(arg) for arg in node[2:end]}...)
-    end)
-
-println()
-ex = code_pattern_dispatch(mt_scprop, :scprop)
-pprintln(ex)
-
-eval(ex)
-
-
-mt_propsc = PatternMethodTable()
-add(mt_propsc, :( callnode(op, arg1, arg2)), :(
-        callnode(op, propsc(arg1), propsc(arg2)) ))
-add(mt_propsc, :( (inputnode(name)) ), :( refnode(inputnode(name), :(...)) ))
-add(mt_propsc, :(node), :(
-        tuple(node[1],{scatterprop(scatter(arg)) for arg in node[2:end]}...) ))
-
-ex2 = code_pattern_dispatch(mt_propsc, :propsc)
-eval(ex2)
-
-# @pattern function propsc(callnode(op, arg1, arg2))
-#     callnode(op, propsc(arg1), propsc(arg2))
-# end
-# @pattern propsc(inputnode(name)) = refnode(inputnode(name), :(...))
-# @pattern function propsc(node)
-#     tuple(node[1], {scatterprop(scatter(arg)) for arg in node[2:end]}...)
-# end
-
-
-pprintln()
-spex2 = scprop(scatternode(X))
-pprintln(spex2)
